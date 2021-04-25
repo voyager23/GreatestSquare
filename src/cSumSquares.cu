00fce0e6bccac617061eaca039e45f1f4865db46
Mike <mike@nowhere.com>
2021-04-25 17:47:08 +0200

    Restore point for cSumSquares.cu

--- src/cSumSquares.cu
+++ src/cSumSquares.cu
@@ -23,6 +23,11 @@
 #include <math.h>
 #include <cuda.h>
 
+__global__ void set_squares(long *d_squares, long n_squares) {
+	int i = threadIdx.x;
+	if(i < n_squares) d_squares[i] = (long)(i+1)*(i+1);
+}
+
 __global__ void kernel(ulong* d_squares, const ulong n_squares, ulong* d_results, ulong N) {
 	ulong i = threadIdx.x + (blockIdx.x * blockDim.x);
 	if(i < N) {
@@ -44,6 +49,7 @@ __global__ void kernel(ulong* d_squares, const ulong n_squares, ulong* d_results
 int main(int argc, char **argv)
 {
 	cudaError_t error_id;
+	long *d_squares;
 	
 	// extract target N
 	ulong x = 0;
@@ -53,19 +59,39 @@ int main(int argc, char **argv)
 		printf("usage: css target (< 10^9)\n");
 		exit(1);
 	}	
-	const ulong N = x;
+	const long N = x;
 	if(N <= 1000000000L) {
 		printf("target: %ld\n", N);
 	} else {
 		printf("target: %ld exceeds program limitations\n", N);
 		exit(2);
 	}
-	// calc. and copy table of perfect squares to device
 	// determine array dimensions
-	double limit = sqrt(sqrt(N)) + 1;
-	printf("target: %ld		limit: %ld\n", N, (ulong)limit);
+	long limit = (long)(sqrt(sqrt(N)) + 1);	// defines size of array	
+		// debug
+		printf("target: %ld		limit: %ld\n", N, limit);
+	// Allocate space on device
+	error_id = cudaMalloc(&d_squares, sizeof(long)*limit);
+	if(error_id != cudaSuccess) {
+		printf("cudaMalloc squares failed with %d\n", error_id);
+		exit(1);
+	}	
+	// launch the generator on kernel
+	set_squares<<<1,limit>>>(d_squares, limit);
+		// debug
+		cudaDeviceSynchronize();
+		// allocate space on host and copy device squares
+		long *h_squares = (long*)malloc(sizeof(long)*limit);
+		cudaMemcpy(h_squares, d_squares, sizeof(long)*limit, cudaMemcpyDeviceToHost);
+		// print array
+		for(int x = 0; x < limit; ++x) printf("%d:%ld  ", x, h_squares[x]); printf("\n");
+		// clear host array
+		free(h_squares);
+	
+	
 
 	//-------
+	cudaFree(d_squares);
 	return 0;
 }
 
