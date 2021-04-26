fac6235fb7bdf76769c622b062f7ae8f6463bda6
Mike <mike@nowhere.com>
2021-04-26 09:40:21 +0200

    Restore point for incomplete code. If the target N exceeds 1e8 the memory on device is too small. This implies multiple passes to calc result?

--- src/cSumSquares.cu
+++ src/cSumSquares.cu
@@ -1,5 +1,5 @@
 /*
- * bSumSquares.cu
+ * cSumSquares.cu
  * 
  * Copyright 2021 mike <mike@fedora33>
  * 
@@ -18,30 +18,32 @@
  * =====================================================================
  */
 
-
 #include <stdio.h>
 #include <math.h>
 #include <cuda.h>
 
+#define DEBUG 1
+#define MANAGED 0
+
 __global__ void set_squares(long *d_squares, long n_squares) {
-	int i = threadIdx.x;
-	if(i < n_squares) d_squares[i] = (long)(i+1)*(i+1);
+	long i = threadIdx.x + (blockIdx.x * blockDim.x);
+	if(i < n_squares) d_squares[i] = (int)(i+1)*(i+1);
 }
 
-__global__ void kernel(ulong* d_squares, const ulong n_squares, ulong* d_results, ulong N) {
-	ulong i = threadIdx.x + (blockIdx.x * blockDim.x);
+__global__ void func_g(int* d_squares, const long limit, long *h_sums, long N) {
+	long i = threadIdx.x + (blockDim.x * blockIdx.x);
 	if(i < N) {
 		// scan in reverse the squares array
 		// save first square which divides i in results[i]
 		if(i > 3) {
-			for(int x = n_squares-1; x > 0; x -= 1) {
+			for(long x = limit-1; x > 0; x -= 1) {
 				if((i % d_squares[x]) == 0) {
-					d_results[i] = d_squares[x];
+					h_sums[i] = d_squares[x];
 					break;
 				}
 			} // for...
 		} else {
-			d_results[i] = i;
+			h_sums[i] = i;
 		}
 	} //
 }
@@ -49,14 +51,15 @@ __global__ void kernel(ulong* d_squares, const ulong n_squares, ulong* d_results
 int main(int argc, char **argv)
 {
 	cudaError_t error_id;
-	long *d_squares;
+	long *d_squares = NULL;
+	long *h_sums = NULL;
 	
 	// extract target N
-	ulong x = 0;
+	long x = 0;
 	if(argc == 2) {
 		x = atol(argv[1]);
 	} else {
-		printf("usage: css target (< 10^9)\n");
+		printf("usage: css target (< 1e8)\n");
 		exit(1);
 	}	
 	const long N = x;
@@ -68,30 +71,58 @@ int main(int argc, char **argv)
 	}
 	// determine array dimensions
 	long limit = (long)(sqrt(sqrt(N)) + 1);	// defines size of array	
-		// debug
+
+#if(DEBUG)
 		printf("target: %ld		limit: %ld\n", N, limit);
+#endif
+
 	// Allocate space on device
-	error_id = cudaMalloc(&d_squares, sizeof(long)*limit);
+	error_id = cudaMalloc(&d_squares, sizeof(long )*limit);
 	if(error_id != cudaSuccess) {
 		printf("cudaMalloc squares failed with %d\n", error_id);
 		exit(1);
 	}	
 	// launch the generator on kernel
 	set_squares<<<1,limit>>>(d_squares, limit);
-		// debug
-		cudaDeviceSynchronize();
+	cudaDeviceSynchronize();
+
+#if(DEBUG)	
 		// allocate space on host and copy device squares
-		long *h_squares = (long*)malloc(sizeof(long)*limit);
-		cudaMemcpy(h_squares, d_squares, sizeof(long)*limit, cudaMemcpyDeviceToHost);
-		// print array
-		for(int x = 0; x < limit; ++x) printf("%d:%ld  ", x, h_squares[x]); printf("\n");
+		long *h_squares = (long *)malloc(sizeof(long )*limit);
+		cudaMemcpy(h_squares, d_squares, sizeof(long )*limit, cudaMemcpyDeviceToHost);
+		// prlong array
+		for(long x = 0; x < limit; ++x) printf("%d:%ld  ", x, h_squares[x]); printf("\n");
 		// clear host array
 		free(h_squares);
+#endif
+
+#if(MANAGED)
+	// Allocate managed memory for results
+	// Note: Fails for more than 1e8 long int
+	error_id = cudaMallocManaged(&h_sums, sizeof(long)*N);
+	if(error_id != cudaSuccess) {
+		printf("cudaMallocManaged sums failed with %d\n", error_id);
+		exit(1);
+	}
+#else
+	h_sums = (long*)malloc(sizeof(long)*N);
+	if(h_sums == NULL){
+		printf("malloc() h_sums failed.\n");
+		exit(1);
+	}
+#endif
 	
+	
+	
+	// calculate the launch config based in thread blocks of 1024 threads
+	const int nBlocks = (N/1024) + 1;
+	// launch the kernel
+	//func_g<<<nBlocks,1024>>()
 	
 
 	//-------
 	cudaFree(d_squares);
+	cudaFree(h_sums);
 	return 0;
 }
 
